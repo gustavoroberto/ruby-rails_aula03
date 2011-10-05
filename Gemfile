d674345f176e34495e94659c4d014bc18b798536
Gustavo <gustavo@ubuntu.ubuntu-domain>
2011-10-05 09:20:15 -0300

    Projeto novo de Rails - Agenda

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+gem 'therubyracer' # javascript (coffescript)
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agenda::Application.config.secret_token = 'ebddb366e09e7c52070ce768c488451a75a8849d5d6fa9181cfcb6c8290b7e2fe008c4bfef664817b90aac45d8d7a25fe40b66acb6835f9c91098acbb6029fb7'
